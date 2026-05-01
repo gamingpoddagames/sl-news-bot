978582730908796d5b4a1375879a82e63a3d8eb4
CoreFlow <gamingpoddagames@gmail.com>
2026-05-02 02:30:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,30 @@
+import os
 import requests
 
-BOT_TOKEN = "7964042596:AAERuaP19P5gpHdxNQxjVh-L9SCjYn6MGkU"
-CHANNEL_USERNAME = "@newssl247_bot"
+BOT_TOKEN = os.environ["7964042596:AAERuaP19P5gpHdxNQxjVh-L9SCjYn6MGkU"]
+CHANNEL = os.environ["@slnews247"]
 
-NEWS_API = "https://newsapi.org/v2/top-headlines?country=us&apiKey=fc1399b9a22944cca99206c268a68a06"
+NEWS_API = os.environ["fc1399b9a22944cca99206c268a68a06"]
 
 def get_news():
     res = requests.get(NEWS_API).json()
     articles = res.get("articles", [])[:5]
 
-    message = "🚨 *SL BREAKING NEWS 24/7*\n\n"
+    msg = "🚨 SL BREAKING NEWS 24/7\n\n"
 
     for a in articles:
-        title = a.get("title", "No title")
-        url = a.get("url", "")
-        message += f"• {title}\n{url}\n\n"
+        title = a.get("title")
+        url = a.get("url")
+        msg += f"• {title}\n{url}\n\n"
 
-    return message
+    return msg
 
 def send(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {
-        "chat_id": CHANNEL_USERNAME,
-        "text": msg,
-        "parse_mode": "Markdown"
-    }
-    requests.post(url, data=data)
+    requests.post(url, data={
+        "chat_id": CHANNEL,
+        "text": msg
+    })
 
 if __name__ == "__main__":
     send(get_news())
